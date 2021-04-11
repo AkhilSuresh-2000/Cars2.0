7a2c4ecc547d6141c173d3b595109032375778cb
Akhil Suresh <akhilsuresh2456@gmail.com>
2021-04-11 11:41:40 -0400

    Add project files.

---
+++ Cars2.0/AppDbContext/CarsDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Cars2._0.Models;
+
+
+
+namespace Cars2._0.AppDbContext
+{
+    public class CarsDbContext: DbContext
+    {
+        public CarsDbContext(DbContextOptions<CarsDbContext> options):
+            base(options)
+        {
+
+        }
+        public DbSet<Make> Makes { get; set; }
+        public DbSet<Model> Models { get; set; }
+    }
+}
---
+++ Cars2.0/Models/Model.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cars2._0.Models
+{
+    public class Model
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        //inverse property 
+        
+        public Make Make { get; set; }
+
+        //foriegn key
+       
+        public int MakeID { get; set; }
+    }
+}
